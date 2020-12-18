904e8b2eb9b0ec88d8b87e43f98ead662d4ab62c
edxu96 <edxu96@outlook.com>
2020-12-18 07:31:35 +0100

    Smaller figure for ACF and PACF (#109)

--- elec_consumption/utils.py
+++ elec_consumption/utils.py
@@ -1,8 +1,10 @@
 """Utility functions."""
 import time
-from typing import Tuple
+from typing import Optional, Tuple
 
+import matplotlib.pyplot as plt
 from pandas.core.series import Series
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
 class ContextTimer():
@@ -37,3 +39,16 @@ def cal_increment_percent(series: Series) -> Tuple[float, float, float]:
     _min = series.min()
     _percent = abs((_max - _min) / _min) * 100
     return _percent, _max, _min
+
+
+def plot_acf_pacf(series: Series, whe_ylim: Optional[bool] = True):
+    _, (ax1, ax2) = plt.subplots(2, 1)
+    plot_acf(series, lags=60, ax=ax1);
+    ax1.set_xlim([0.5, 60.5]);
+    if whe_ylim:
+        ax1.set_ylim([-0.3, 0.3]);
+
+    plot_pacf(series, lags=60, ax=ax2);
+    ax2.set_xlim([0.5, 60.5]);
+    if whe_ylim:
+        ax2.set_ylim([-0.3, 0.3]);
